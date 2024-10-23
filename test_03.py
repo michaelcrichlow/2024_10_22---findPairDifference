f8456dfb7ef7716369a108aaa3e84f9092102a56
Michael Crichlow <mikecrichlow@gmail.com>
2024-10-22 18:11:48 -0700

    Update test_03.py

--- test_03.py
+++ test_03.py
@@ -5,14 +5,14 @@ def findPairDifference(l: list[int], n: int) -> list[int]:
     for val in l:
         if n >= val:
             if val + n not in local_set:
-                local_set.add(val + n)
+                local_set.add(val)
             else:
                 local_array.append(val)
                 local_array.append(val + n)
                 break
 
         elif n < val:
-            if val not in local_set:
+            if val - n not in local_set:
                 local_set.add(val)
             else:
                 local_array.append(val)
@@ -29,9 +29,8 @@ def main() -> None:
     print(findPairDifference([1, 7, 5, 9, 2, 12, 3], 7))
     print(findPairDifference([1, 7, 5, 9, 2, 12, 3], 9))
     print(findPairDifference([1, 7, 5, 9, 2, 12, 3], 30))
+    print(findPairDifference([3, 3], 2))
 
 
 if __name__ == '__main__':
     main()
-
-# findPairDifference([1, 7, 5, 9, 2, 12, 3], 2) => [1, 3]
